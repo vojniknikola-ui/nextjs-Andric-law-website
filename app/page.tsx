eb032d51420100200a3b1b70b945853e594b38fd
vojniknikola-ui <vojniknikola@gmail.com>
2026-01-12 16:23:22 +0100

    fix useState import - build success

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useState } from 'react';
 import { Header } from '@/components/Header';
 import { Footer } from '@/components/Footer';
 import { ServicesSection } from '@/components/ServicesSection';
